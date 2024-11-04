3f665ddfdfc3b1025d6aa3981275faf788a2fdae
Basri Çiftçi <ciftcibasri@gmail.com>
2024-11-04 04:38:38 +0300

    first commit

---
+++ nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,39 @@
+return {
+    {  "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {"lua_ls","gopls","pylsp", "yamlls"}
+            })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function ()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({})
+            lspconfig.gopls.setup({
+                settings = {
+                    gopls = {
+                        analyses = {
+                            unusedparams = true,
+                        },
+                        staticcheck = true,
+                        gofumpt = true
+                    }
+                }
+            })
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+            vim.keymap.set({"n"}, "<leader>ca", vim.lsp.buf.code_action, {})
+
+        end
+    }
+}
---
+++ nvim/lua/plugins/none-ls.lua
@@ -0,0 +1,23 @@
+return {
+    "nvimtools/none-ls.nvim",
+    config = function()
+        local null_ls = require("null-ls")
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.diagnostics.pylint.with({
+                    diagnostics_postprocess = function(diagnostic)
+                        diagnostic.code = diagnostic.message_id
+                    end,
+                }),
+                null_ls.builtins.formatting.isort,
+                null_ls.builtins.formatting.black,
+                null_ls.builtins.formatting.gofmt,
+                null_ls.builtins.formatting.goimports,
+                null_ls.builtins.formatting.prettier,
+
+            },
+        })
+        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    end,
+}
